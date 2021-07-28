4dce5caf3d90f288860a51eaa755519615425474
nirlep5252 <hellonirlep@gmail.com>
2021-07-28 15:38:52 +0530

    mother fucker font

--- epicbot_images/memes.py
+++ epicbot_images/memes.py
@@ -1,7 +1,6 @@
 from PIL import Image, ImageDraw, ImageFont
 from .utils import wrap_text
 
-font = ImageFont.truetype("assets\\fonts\Roboto-Bold.ttf", size=30)
 templates = "assets\meme_templates"
 
 async def drake(first, second) -> bytes:
@@ -11,6 +10,7 @@ async def drake(first, second) -> bytes:
     _f = wrap_text(15, first)
     _s = wrap_text(15, second)
     with Image.open(f"{templates}\drake.png") as _t:
+        font = ImageFont.truetype("assets\fonts\Roboto-Bold.ttf", size=30)
         t = _t.copy()
         draw = ImageDraw.Draw(t)
         draw.multiline_text(xy=(435, 150), text=_f, font=font, fill=(0, 0, 0), anchor='mm', align='center')
