45f9d2b698785b6baeeba0169e62563c4b22b0e5
nirlep5252 <hellonirlep@gmail.com>
2021-07-28 16:51:15 +0530

    aiuhduagwd

--- epicbot_images/memes.py
+++ epicbot_images/memes.py
@@ -1,8 +1,9 @@
 from PIL import Image, ImageDraw, ImageFont
 from .utils import wrap_text
-import pkgutil
+import pathlib
 
 templates = "assets/meme_templates"
+current_path = pathlib.Path(__file__).parent.resolve()
 
 async def drake(first, second) -> bytes:
     """
@@ -10,8 +11,8 @@ async def drake(first, second) -> bytes:
     """
     _f = wrap_text(15, first)
     _s = wrap_text(15, second)
-    with Image.open(pkgutil.get_data(__name__, f"{templates}/drake.png")) as _t:
-        font = ImageFont.truetype(pkgutil.get_data(__name__, "assets/fonts/Roboto-Bold.ttf"), size=30)
+    with Image.open(f"{current_path}\{templates}\drake.png") as _t:
+        font = ImageFont.truetype(f"{current_path}\\assets\\fonts\Roboto-Bold.ttf", size=30)
         t = _t.copy()
         draw = ImageDraw.Draw(t)
         draw.multiline_text(xy=(435, 150), text=_f, font=font, fill=(0, 0, 0), anchor='mm', align='center')
