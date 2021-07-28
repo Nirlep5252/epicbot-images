37c02f61cb44cc32a4b53184f237da5583b1ecea
nirlep5252 <hellonirlep@gmail.com>
2021-07-28 15:19:16 +0530

    hmm

--- epicbot_images/memes.py
+++ epicbot_images/memes.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageDraw, ImageFont
 from .utils import wrap_text
 
 font = ImageFont.truetype("assets/fonts/Roboto-Bold.ttf", size=30)
-templates = "assets/meme_templates/"
+templates = "assets\meme_templates"
 
 async def drake(first, second) -> bytes:
     """
@@ -10,7 +10,7 @@ async def drake(first, second) -> bytes:
     """
     _f = wrap_text(15, first)
     _s = wrap_text(15, second)
-    with Image.open(f"{templates}drake.png") as _t:
+    with Image.open(f"{templates}\drake.png") as _t:
         t = _t.copy()
         draw = ImageDraw.Draw(t)
         draw.multiline_text(xy=(435, 150), text=_f, font=font, fill=(0, 0, 0), anchor='mm', align='center')
