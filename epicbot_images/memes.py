c1b5f958bbab6a26b24e05a4ce410f78544cf45b
nirlep5252 <hellonirlep@gmail.com>
2021-07-29 08:46:56 +0530

    added the remaining memes

--- epicbot_images/memes.py
+++ epicbot_images/memes.py
@@ -13,7 +13,9 @@ import pathlib
 
 templates = "assets/meme_templates"
 current_path = pathlib.Path(__file__).parent.resolve()
-font = ImageFont.truetype(f"{current_path}/assets/fonts/Roboto-Bold.ttf", size=30)
+
+def font(size: int = 30, fontname: str = 'Roboto-Bold'):
+    return ImageFont.truetype(f"{current_path}/assets/fonts/{fontname}.ttf", size=size)
 
 async def drake(first: str, second: str) -> str:
     """
@@ -24,8 +26,8 @@ async def drake(first: str, second: str) -> str:
     with Image.open(f"{current_path}/{templates}/drake.png") as _t:
         t = _t.copy()
         draw = ImageDraw.Draw(t)
-        draw.multiline_text((435, 150), _f, (0, 0, 0), font, 'mm', align='center')
-        draw.multiline_text((435, 435), _s, (0, 0, 0), font, 'mm', align='center')
+        draw.multiline_text((435, 150), _f, (0, 0, 0), font(), 'mm', align='center')
+        draw.multiline_text((435, 435), _s, (0, 0, 0), font(), 'mm', align='center')
         save_path = f"{current_path}/temp/drake.png"
         t.save(save_path)
         return save_path
@@ -39,8 +41,8 @@ async def disappointed(first: str, second: str) -> str:
     with Image.open(f"{current_path}/{templates}/disappointed.png") as _t:
         t = _t.copy()
         draw = ImageDraw.Draw(t)
-        draw.multiline_text((250, 120), _f, (0, 0, 0), font, 'mm', align='center')
-        draw.multiline_text((250, 400), _s, (0, 0, 0), font, 'mm', align='center')
+        draw.multiline_text((250, 120), _f, (0, 0, 0), font(), 'mm', align='center')
+        draw.multiline_text((250, 400), _s, (0, 0, 0), font(), 'mm', align='center')
         save_path = f"{current_path}/temp/disappointed.png"
         t.save(save_path)
         return save_path
@@ -52,14 +54,15 @@ async def flex_tape(first: str, second: str, username: str = None) -> str:
     """
     _f = wrap_text(15, first)
     _s = wrap_text(15, second)
-    _u = wrap_text(25, username)
+    if username is not None:
+        _u = wrap_text(25, username)
     with Image.open(f"{current_path}/{templates}/flex_tape.png") as _t:
         t = _t.copy()
         draw = ImageDraw.Draw(t)
-        draw.multiline_text((490, 150), _f, (255, 255, 255), font, 'mm', align='center')
-        draw.multiline_text((420, 500), _s, (255, 255, 255), font, 'mm', align='center')
+        draw.multiline_text((490, 150), _f, (255, 255, 255), font(), 'mm', align='center')
+        draw.multiline_text((420, 500), _s, (255, 255, 255), font(), 'mm', align='center')
         if username is not None:
-            draw.multiline_text((200, 175), _u, (255, 255, 255), font, 'ms', align='center')
+            draw.multiline_text((200, 175), _u, (255, 255, 255), font(), 'ms', align='center')
         save_path = f"{current_path}/temp/flex_tape.png"
         t.save(save_path)
         return save_path
@@ -72,8 +75,40 @@ async def bernie(text: str) -> str:
     with Image.open(f"{current_path}/{templates}/bernie.png") as _t:
         t = _t.copy()
         draw = ImageDraw.Draw(t)
-        draw.multiline_text((350, 670), _e, (255, 255, 255), font, 'ms', align='center')
+        draw.multiline_text((350, 670), _e, (255, 255, 255), font(40), 'ms', align='center')
         save_path = f"{current_path}/temp/bernie.png"
+        t.save(save_path)
+        return save_path
+
+async def doge(first: str, second: str) -> str:
+    """
+    Makes the doge meme.
+    """
+    _f = wrap_text(20, first)
+    _s = wrap_text(20, second)
+    with Image.open(f"{current_path}/{templates}/doge.png") as _t:
+        t = _t.copy()
+        draw = ImageDraw.Draw(t)
+        draw.multiline_text((150, 370), _f, (0, 0, 0), font(), 'ms', align='center')
+        draw.multiline_text((450, 370), _s, (0, 0, 0), font(), 'ms', align='center')
+        save_path = f"{current_path}/temp/doge.png"
+        t.save(save_path)
+        return save_path
+
+async def panik(panic: str, kalm: str, panik: str) -> str:
+    """
+    Panic... Kalm... PANIKK!!!
+    """
+    panic = wrap_text(15, panic)
+    kalm = wrap_text(15, kalm)
+    panik = wrap_text(15, panik)
+    with Image.open(f"{current_path}/{templates}/panik.png") as _t:
+        t = _t.copy()
+        draw = ImageDraw.Draw(t)
+        draw.multiline_text((133, 133), panic, (0, 0, 0), font(), 'mm', align='center')
+        draw.multiline_text((133, 435), kalm, (0, 0, 0), font(), 'mm', align='center')
+        draw.multiline_text((133, 730), panik, (0, 0, 0), font(), 'mm', align='center')
+        save_path = f"{current_path}/temp/panik.png"
         t.save(save_path)
         return save_path
 
